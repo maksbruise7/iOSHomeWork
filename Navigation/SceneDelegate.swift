26ce77be7d534b97239e23f569cc7abe84682248
maksbruise7 <sinelnik0709@icloud.com>
2026-06-10 17:06:23 +0300

    Viper

--- Navigation/SceneDelegate.swift
+++ Navigation/SceneDelegate.swift
@@ -1,8 +1,9 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
+    var appCoordinator: AppCoordinator?
     
     func scene(
         _ scene: UIScene,
@@ -11,39 +12,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     ) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
-        window = UIWindow(windowScene: windowScene)
+        let window = UIWindow(windowScene: windowScene)
+        let navigationController = UINavigationController()
         
-        let imageOne = UIImage(named: "Book")
-        let imageTwo = UIImage(named: "Horse")
+        appCoordinator = AppCoordinator(navigationController: navigationController)
+        appCoordinator?.start()
         
-        let tabbarController = UITabBarController()
-        
-        // MARK: - Feed Tab
-        let feedVC = FeedViewController()
-        let feedNavController = UINavigationController(rootViewController: feedVC)
-        feedNavController.tabBarItem = UITabBarItem(title: "Feed", image: imageOne, tag: 0)
-        
-        // MARK: - Profile Tab (MVVM)
-        #if DEBUG
-        let users = DataProvider.getTestUsers()
-        let userService: UserService = TestUserService(users: users)
-        let userLogin = users.first?.login ?? "test_user"
-        #else
-        let users = DataProvider.getRealUsers()
-        let userService: UserService = CurrentUserService(users: users)
-        let userLogin = users.first?.login ?? "admin"
-        #endif
-        
-        let profileViewModel = ProfileViewModel(userService: userService, userLogin: userLogin)
-        let profileVC = ProfileViewController()
-        profileVC.viewModel = profileViewModel
-        
-        let profileNavController = UINavigationController(rootViewController: profileVC)
-        profileNavController.tabBarItem = UITabBarItem(title: "Profile", image: imageTwo, tag: 1)
-        
-        tabbarController.viewControllers = [feedNavController, profileNavController]
-        
-        window?.rootViewController = tabbarController
-        window?.makeKeyAndVisible()
+        window.rootViewController = navigationController
+        self.window = window
+        window.makeKeyAndVisible()
     }
 }
